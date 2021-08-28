9d2a69e61cfd185b18e52a9464865fdb521783ed
Anusha Kansal <59743854+anushakansal@users.noreply.github.com>
2021-08-28 23:34:07 +0530

    updated cors whitelist

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://rails-ads-app.netlify.app'
+    origins 'http://localhost:3000', 'https://ads-client.netlify.app'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete], credentials: true
   end
 end
